495716fe2fc7b9f2e2bb7973949dda1ccc4ad54e
Ivan Retamozo <100177931+ivanretamozodev@users.noreply.github.com>
2022-12-15 10:34:11 -0300

    configuracion prettier y final

--- index.js
+++ index.js
@@ -45,61 +45,48 @@ console.log('will read file');
 ////////////////////////////////////
 /// SERVER
 
-const tempOverview = fs.readFileSync(
-    `${__dirname}/templates/template-overview.html`,
-    'utf-8'
-);
-
-const tempProduct = fs.readFileSync(
-    `${__dirname}/templates/template-product.html`,
-    'utf-8'
-);
-const tempCard = fs.readFileSync(
-    `${__dirname}/templates/template-card.html`,
-    'utf-8'
-);
-
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    /* console.log(res.url); */
-    const { query, pathname } = url.parse(req.url, true);
-    if (pathname === '/' || pathname === '/overview') {
-        //Overview page
-        res.writeHead(200, {
-            'Content-type': 'text/html',
-        });
+  /* console.log(res.url); */
+  const { query, pathname } = url.parse(req.url, true);
+  if (pathname === '/' || pathname === '/overview') {
+    //Overview page
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
 
-        const cardsHtml = dataObj
-            .map((el) => replaceTemplate(tempCard, el))
-            .join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-        res.end(output);
-    } else if (pathname === '/product') {
-        //product page
-        res.writeHead(200, {
-            'Content-type': 'text/html',
-        });
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct, product);
-        res.end(output);
-    } else if (pathname === '/api') {
-        //api page
-        res.writeHead(200, {
-            'Content-type': 'application/json',
-        });
-        res.end(data);
-    } else {
-        //not found page
-        res.writeHead(404, {
-            'content-type': 'text/html',
-        });
-        res.end('Page not found :C');
-    }
+    res.end(output);
+  } else if (pathname === '/product') {
+    //product page
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  } else if (pathname === '/api') {
+    //api page
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
+  } else {
+    //not found page
+    res.writeHead(404, {
+      'content-type': 'text/html',
+    });
+    res.end('Page not found :C');
+  }
 });
 
 server.listen(3000, '127.0.0.1', () => {
-    console.log('server is running at PORT=3000!');
+  console.log('server is running at PORT=3000!');
 });
